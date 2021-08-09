b1f7c7d468749080d28ab5098ad2a759ff066fbc
Ali-F2 <you@example.com>
2021-08-08 22:01:48 -0500

    Configurar archivo package.json

---
+++ Recetario/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html'
+    })
+  ]
+}
